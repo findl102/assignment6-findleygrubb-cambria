4638241f9a782f33db7a9456cfa7369a36bdb925
Cambria Findley-Grubb <cfindleygrubb@ufl.edu>
2019-04-19 18:58:34 -0500

    Fixing errors in Java code

--- assignment6/scripts/main.js
+++ assignment6/scripts/main.js
@@ -1,12 +1,10 @@
-var members = ['Dave Grohl', 'Nate Mendel', 'Pat Smear', 'Taylor Hawkins', 'Rami Jaffee', 'Chris Shiflett'];
+var members = [ 'Dave Grohl', 'Nate Mendel', 'Pat Smear', 'Taylor Hawkins', 'Rami Jaffee', 'Chris Shiflett' ];
 
 var tour = {
   Chicago: "July 29th and July 30",
   Seattle: "September 1st",
   Edmonton: "September 4th",
 };
-
-
 
 var i = 0;
 
