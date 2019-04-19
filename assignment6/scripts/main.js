44adf126b18b6397f9219405dad7c04188875420
Cambria Findley-Grubb <cfindleygrubb@ufl.edu>
2019-04-19 18:27:51 -0500

    Adjusted java file for assignment with code

---
+++ assignment6/scripts/main.js
@@ -0,0 +1,17 @@
+var members = ['Dave Grohl', 'Nate Mendel', 'Pat Smear', 'Taylor Hawkins', 'Rami Jaffee', 'Chris Shiflett'];
+
+var tour = {
+  Chicago: "July 29th and July 30",
+  Seattle: "September 1st",
+  Edmonton: "September 4th",
+};
+
+
+
+var i = 0;
+
+while (i < 5) {
+  return('Times running through the loop' + i);
+
+  i++;
+}
